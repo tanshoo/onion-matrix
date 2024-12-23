01926dcf8410876f28bda7d7001052df16ff54b1
Ewa Kaluś <ewakalus1@gmail.com>
2024-12-23 18:38:38 +0100

    Implement solve in OnionMatrix

--- OnionMatrix.py
+++ OnionMatrix.py
@@ -39,4 +39,17 @@ class OnionMatrix:
 
 
     def solve(self, b: np.ndarray) -> np.ndarray:
-        pass
+        """
+        Solve the system of equations Ax = b, where A is the onion matrix.
+        :param b: Vector of length n*m
+        :return: The solution x
+        """
+
+        block_diffs = np.diff(self.blocks, axis=0, prepend=np.zeros((1, self.m, self.m)))
+        b_diffs = np.diff(b.reshape(self.n, self.m), axis=0, prepend=np.zeros((1, self.m)))
+        res_suffix_sums = np.linalg.solve(block_diffs, b_diffs)
+        res_suffix_sums = np.vstack((res_suffix_sums, np.zeros((1, self.m))))
+        res = res_suffix_sums[:-1] - res_suffix_sums[1:]
+
+        return res.ravel()
+
