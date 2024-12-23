a8a3a75bb138430b236db2f43db9c3132d10d497
Ewa Kaluś <ewakalus1@gmail.com>
2024-12-23 17:42:27 +0100

    Optimize multiply using np.einsum, use ravel instead of flatten (~0.000238 s benchmark)

--- OnionMatrix.py
+++ OnionMatrix.py
@@ -10,13 +10,13 @@ class OnionMatrix:
     are correct and does not perform any validation.
     """
 
-    blocks: list[np.ndarray]
+    blocks: np.ndarray
     n: int 
     m: int 
 
 
     def __init__(self, blocks: list[np.ndarray]) -> None:
-        self.blocks = blocks
+        self.blocks = np.array(blocks)
         self.n = len(blocks)
         self.m = len(blocks[0])
 
@@ -29,16 +29,13 @@ class OnionMatrix:
         """
 
         v_blocks = v.reshape(self.n, self.m)
-        diagonal_blocks = np.array([self.blocks[i] @ v_blocks[i] for i in range(self.n)])
+        diagonal_blocks = np.einsum('ijk,ik->ij', self.blocks, v_blocks)
         prefix_sums = np.cumsum(diagonal_blocks, axis=0)
         suffix_vector_sums = np.cumsum(v_blocks[::-1], axis=0)[::-1]
-        result_blocks = (
-            prefix_sums 
-            + np.array([self.blocks[i] @ suffix_vector_sums[i] for i in range(self.n)]) 
-            - diagonal_blocks
-        )
+        suffix_sums = np.einsum('ijk,ik->ij', self.blocks, suffix_vector_sums)
+        result_blocks = prefix_sums + suffix_sums - diagonal_blocks
 
-        return result_blocks.flatten()
+        return result_blocks.ravel()
 
 
     def solve(self, b: np.ndarray) -> np.ndarray:
